cf2145fa0ffe414b90210ee36ee4d86214a4ab7f
vandosant <sjskender@gmail.com>
2014-12-23 11:14:44 -0700

    Users can add an article

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,12 @@
-angular.module('angularCrud', []);
+angular.module('angularCrud', [])
+  .controller("FormController", ['$scope', function ($scope) {
+    $scope.articles = [];
+
+    $scope.update = function (article) {
+      console.log($scope.articles);
+      console.log(article);
+
+      $scope.articles.push(article);
+    }
+
+  }]);
--- spec/spec.js
+++ spec/spec.js
@@ -4,4 +4,13 @@ describe('app home', function () {
     result = element(by.css('.hello'));
     expect(result.getText()).toEqual('hello');
   });
+
+  it('should allow users to add an article', function () {
+    browser.get('http://localhost:3000/');
+    element(by.model('article.url')).sendKeys('http://test.com');
+    submit = element(by.css('input[type="submit"'));
+    submit.click();
+    urls = element.all(by.repeater('a in articles'));
+    expect(urls.count()).toBe(1);
+  });
 });
