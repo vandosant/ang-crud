ed479bc4d4698c12a5da848a50b93a7786d3aa09
vandosant <sjskender@gmail.com>
2014-12-23 11:51:36 -0700

    Wired up to use the mongodb

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,15 @@
 angular.module('angularCrud', [])
-  .controller("FormController", ['$scope', function ($scope) {
+  .controller("FormController", ['$scope', '$http', function ($scope, $http) {
     $scope.articles = [];
 
     $scope.update = function (article) {
       console.log($scope.articles);
       console.log(article);
 
-      $scope.articles.push(article);
+      $http.post('/articles', article).success(function(data) {
+        console.log(data);
+        $scope.articles.push(data);
+      })
     }
 
   }]);
